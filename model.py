5e46c735a89ed34400b5ba902103aa556c387f07
LazyKernel <miksu.fredriksson@hotmail.com>
2023-04-29 01:42:02 +0300

    Use serial connection instead of sdr

--- model.py
+++ model.py
@@ -23,7 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import numpy as np
-from rtlsdr import *
+import serial
 
 import freqshow
 
@@ -41,9 +41,13 @@ class FreqShowModel(object):
 		self.max_auto_scale = True
 		self.set_min_intensity('AUTO')
 		self.set_max_intensity('AUTO')
+		# init serial connection
+		self.ser = serial.Serial('/dev/ttyACM0')
+		self.ser.flush()
+
 		# Initialize RTL-SDR library.
-		self.sdr = RtlSdr()
-		self.set_center_freq(90.3)
+		#self.sdr = RtlSdr()
+		self.set_center_freq((2527 - 2400) / 2)
 		self.set_sample_rate(2.4)
 		self.set_gain('AUTO')
 
@@ -56,12 +60,13 @@ class FreqShowModel(object):
 
 	def get_center_freq(self):
 		"""Return center frequency of tuner in megahertz."""
-		return self.sdr.get_center_freq()/1000000.0
+		return (2527 - 2400) / 2
+		#return self.sdr.get_center_freq()/1000000.0
 
 	def set_center_freq(self, freq_mhz):
 		"""Set tuner center frequency to provided megahertz value."""
 		try:
-			self.sdr.set_center_freq(freq_mhz*1000000.0)
+			#self.sdr.set_center_freq(freq_mhz*1000000.0)
 			self._clear_intensity()
 		except IOError:
 			# Error setting value, ignore it for now but in the future consider
@@ -70,12 +75,14 @@ class FreqShowModel(object):
 
 	def get_sample_rate(self):
 		"""Return sample rate of tuner in megahertz."""
-		return self.sdr.get_sample_rate()/1000000.0
+		return 10
+		#return self.sdr.get_sample_rate()/1000000.0
 
 	def set_sample_rate(self, sample_rate_mhz):
 		"""Set tuner sample rate to provided frequency in megahertz."""
 		try:
-			self.sdr.set_sample_rate(sample_rate_mhz*1000000.0)
+			pass
+			#self.sdr.set_sample_rate(sample_rate_mhz*1000000.0)
 		except IOError:
 			# Error setting value, ignore it for now but in the future consider
 			# adding an error message dialog.
@@ -88,19 +95,20 @@ class FreqShowModel(object):
 		if self.auto_gain:
 			return 'AUTO'
 		else:
-			return '{0:0.1f}'.format(self.sdr.get_gain())
+			return 'AUTO'
+			#return '{0:0.1f}'.format(self.sdr.get_gain())
 
 	def set_gain(self, gain_db):
 		"""Set gain of tuner.  Can be the string 'AUTO' for automatic gain
 		or a numeric value in decibels for fixed gain.
 		"""
 		if gain_db == 'AUTO':
-			self.sdr.set_manual_gain_enabled(False)
+			#self.sdr.set_manual_gain_enabled(False)
 			self.auto_gain = True
 			self._clear_intensity()
 		else:
 			try:
-				self.sdr.set_gain(float(gain_db))
+				#self.sdr.set_gain(float(gain_db))
 				self.auto_gain = False
 				self._clear_intensity()
 			except IOError:
@@ -153,28 +161,36 @@ class FreqShowModel(object):
 		values which are the intensities of each frequency bucket (i.e. FFT of
 		radio samples).
 		"""
-		# Get width number of raw samples so the number of frequency bins is
-		# the same as the display width.  Add two because there will be mean/DC
-		# values in the results which are ignored.
-		samples = self.sdr.read_samples(freqshow.SDR_SAMPLE_SIZE)[0:self.width+2]
-		# Run an FFT and take the absolute value to get frequency magnitudes.
-		freqs = np.absolute(np.fft.fft(samples))
-		# Ignore the mean/DC values at the ends.
-		freqs = freqs[1:-1]
-		# Shift FFT result positions to put center frequency in center.
-		freqs = np.fft.fftshift(freqs)
-		# Convert to decibels.
-		freqs = 20.0*np.log10(freqs)
-		# Update model's min and max intensities when auto scaling each value.
-		if self.min_auto_scale:
-			min_intensity = np.min(freqs)
-			self.min_intensity = min_intensity if self.min_intensity is None \
-				else min(min_intensity, self.min_intensity)
-		if self.max_auto_scale:
-			max_intensity = np.max(freqs)
-			self.max_intensity = max_intensity if self.max_intensity is None \
-				else max(max_intensity, self.max_intensity)
-		# Update intensity range (length between min and max intensity).
-		self.range = self.max_intensity - self.min_intensity
-		# Return frequency intensities.
-		return freqs
+		ser_bytes = self.ser.readline()
+		try:
+			decoded_bytes = ser_bytes.decode('utf-8')
+		except:
+			return [0] * 128
+		
+		return [int(v) for v in decoded_bytes.split()]
+
+		# # Get width number of raw samples so the number of frequency bins is
+		# # the same as the display width.  Add two because there will be mean/DC
+		# # values in the results which are ignored.
+		# samples = self.sdr.read_samples(freqshow.SDR_SAMPLE_SIZE)[0:self.width+2]
+		# # Run an FFT and take the absolute value to get frequency magnitudes.
+		# freqs = np.absolute(np.fft.fft(samples))
+		# # Ignore the mean/DC values at the ends.
+		# freqs = freqs[1:-1]
+		# # Shift FFT result positions to put center frequency in center.
+		# freqs = np.fft.fftshift(freqs)
+		# # Convert to decibels.
+		# freqs = 20.0*np.log10(freqs)
+		# # Update model's min and max intensities when auto scaling each value.
+		# if self.min_auto_scale:
+		# 	min_intensity = np.min(freqs)
+		# 	self.min_intensity = min_intensity if self.min_intensity is None \
+		# 		else min(min_intensity, self.min_intensity)
+		# if self.max_auto_scale:
+		# 	max_intensity = np.max(freqs)
+		# 	self.max_intensity = max_intensity if self.max_intensity is None \
+		# 		else max(max_intensity, self.max_intensity)
+		# # Update intensity range (length between min and max intensity).
+		# self.range = self.max_intensity - self.min_intensity
+		# # Return frequency intensities.
+		# return freqs
